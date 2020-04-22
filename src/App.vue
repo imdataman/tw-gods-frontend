39e26318762ddbeee180b366903d70dae94af9f1
Andy Lin <>
2020-04-22 17:00:10 +0800

    refine the viz

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <div id="app">
+    <div class="opening">
+      <h2>台灣廟宇地圖</h2>
+      <p>
+        本文使用內政部的「108年度全國宗教資訊寺廟分布圖」，搭配熱點分析（Hotspot
+        Analysis）技術，分析台灣各主要信仰的地理分佈。地圖的顏色愈深，代表主祀該神祇的廟宇在該地區的廟宇佔比愈高。本文分析六大信仰：福德正神（土地公）、觀世音菩薩、媽祖（天上聖母）、釋迦牟尼、王爺／千歲及玄天上帝。
+      </p>
+    </div>
     <Map />
+    <div class="footnote">
+      <span>
+        資料來源：<a
+          target="_blank"
+          href="https://segis.moi.gov.tw/STAT/Generic/Project/GEN_STAT.ashx?method=downloadproductfile&code=433B6C121D32439A6239FE8CBE94A3D3&STTIME=108Y&STUNIT=null&BOUNDARY=全國"
+          >內政部</a
+        >
+      </span>
+      <span class="sourceCode"
+        >製作：<a href="https://www.facebook.com/imDataMan/" target="_blank"
+          >林佳賢</a
+        ><a href="https://github.com/imdataman/tw-county-square" target="_blank"
+          ><img src="@/assets/GitHub-Mark-32px.png"
+        /></a>
+      </span>
+    </div>
   </div>
 </template>
 
@@ -20,5 +43,43 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  max-width: 1000px;
+  margin: auto;
+}
+
+a {
+  text-decoration: none;
+  color: darkblue;
+  &:hover {
+    color: orange;
+  }
+}
+
+.opening,
+.footnote {
+  padding-left: 10%;
+  padding-right: 10%;
+  max-width: 1000px;
+  p {
+    word-wrap: break-word;
+  }
+  span {
+    font-size: 0.75rem;
+    img {
+      height: 0.75rem;
+      padding-left: 0.25rem;
+      vertical-align: middle;
+    }
+  }
+}
+
+.footnote {
+  padding-bottom: 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.sourceCode {
+  text-align: center;
 }
 </style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -2,8 +2,15 @@
   <div class="map">
     <div class="mapWrapper">
       <div class="mapContainer" v-for="god in godsList" :key="god.id">
-        <div class="mapTitle">
+        <div class="mapText">
           <h3>{{ god.name }}</h3>
+          <ol>
+            <li
+              v-for="(l, i) in god.highlight"
+              :key="`${god.name}${i}`"
+              v-html="`<b>${l.location} - ${l.temple}</b></br>${l.description}`"
+            ></li>
+          </ol>
         </div>
         <svg :viewBox="`0, 0, ${width}, ${height}`">
           <g class="background">
@@ -21,6 +28,25 @@
           </g>
           <g class="outline">
             <path :d="path(outline)"></path>
+          </g>
+          <g
+            class="highlight"
+            v-for="location in god.highlight"
+            :key="location.location"
+          >
+            <text
+              :x="projection(location.coord)[0]"
+              :y="projection(location.coord)[1]"
+              dy="-6"
+            >
+              {{ location.id }}
+            </text>
+            <circle
+              :cx="projection(location.coord)[0]"
+              :cy="projection(location.coord)[1]"
+              r="1.5"
+            ></circle>
+            <!-- <polyline :points="polyline.get(god.id)"></polyline> -->
           </g>
         </svg>
       </div>
@@ -45,16 +71,200 @@ export default {
       grid: topojson.feature(grid, grid.objects["final-grid"]).features,
       county: county,
       godsList: [
-        { name: "福德正神", id: "fude", re: "福德正神|土地公" },
-        { name: "觀世音菩薩", id: "guanyin", re: "觀世音|觀音" },
-        { name: "媽祖", id: "mazu", re: "天上聖母|媽祖" },
-        { name: "釋迦牟尼", id: "buddha", re: "釋迦牟尼|釋迦佛" },
-        { name: "王爺／千歲", id: "wangye", re: "千歲|王爺" },
-        { name: "玄天上帝", id: "xuanwu", re: "玄天|真武|北極大帝" }
+        {
+          name: "福德正神",
+          id: "fude",
+          re: "福德正神|土地公",
+          highlight: [
+            {
+              id: 1,
+              location: "屏東恆春",
+              temple: "高山巖福德宮",
+              description: "台灣最老土地公廟",
+              coord: [120.719604, 21.9633437]
+            },
+            {
+              id: 2,
+              location: "基隆",
+              temple: "福德宮",
+              description: "地方信仰中心",
+              coord: [121.7419687, 25.1280497]
+            },
+            {
+              id: 3,
+              location: "宜蘭四結",
+              temple: "福德廟",
+              description: "台灣最大的金身土地公",
+              coord: [121.7656373, 24.6973611]
+            },
+            {
+              id: 4,
+              location: "台中",
+              temple: "惠來里福德祠",
+              description: "台中市地價最值錢的土地公廟",
+              coord: [120.6442008, 24.1605017]
+            },
+            {
+              id: 5,
+              location: "桃園",
+              temple: "土地公文化館",
+              description: "桃園區土地公廟密度全國第一",
+              coord: [121.321273, 24.9981896]
+            }
+          ]
+        },
+        {
+          name: "觀世音菩薩",
+          id: "guanyin",
+          re: "觀世音|觀音",
+          highlight: [
+            {
+              id: 1,
+              location: "澎湖馬公",
+              temple: "觀音亭",
+              description: "全台唯一由清代澎湖廳總鎮衙署石獅鎮守的寺廟",
+              coord: [119.5620759, 23.5676239]
+            },
+            {
+              id: 2,
+              location: "金門",
+              temple: "靈濟寺",
+              description: "相傳創建於唐朝，已有八百年歷史",
+              coord: [118.3185126, 24.4316216]
+            },
+            {
+              id: 3,
+              location: "新竹北埔",
+              temple: "慈天宮",
+              description: "地方信仰中心",
+              coord: [121.0582618, 24.6997603]
+            }
+          ]
+        },
+        {
+          name: "媽祖",
+          id: "mazu",
+          re: "天上聖母|媽祖",
+          highlight: [
+            {
+              id: 1,
+              location: "彰化芳苑",
+              temple: "普天宮",
+              description: "全台灣面積最大的媽祖廟",
+              coord: [120.3168645, 23.9290597]
+            },
+            {
+              id: 2,
+              location: "高雄旗山",
+              temple: "天后宮",
+              description: "地方信仰中心",
+              coord: [120.4823064, 22.8866163]
+            },
+            {
+              id: 3,
+              location: "台東",
+              temple: "天后宮",
+              description: "以炮炸寒單爺聞名",
+              coord: [121.1554925, 22.7567605]
+            },
+            {
+              id: 4,
+              location: "新北貢寮",
+              temple: "仁和宮",
+              description: "地方信仰中心",
+              coord: [121.9229976, 25.0517402]
+            }
+          ]
+        },
+        {
+          name: "釋迦牟尼",
+          id: "buddha",
+          re: "釋迦牟尼|釋迦佛",
+          highlight: [
+            {
+              id: 1,
+              location: "南投埔里",
+              temple: "中台禪寺",
+              description: "世界最大、最高的佛教寺廟",
+              coord: [120.9445887, 24.0090905]
+            },
+            {
+              id: 2,
+              location: "花蓮",
+              temple: "慈濟文化園區",
+              description: "慈濟全球事業總樞紐",
+              coord: [121.5907229, 23.9938778]
+            },
+            {
+              id: 3,
+              location: "新北金山",
+              temple: "法鼓山",
+              description: "台灣四大名山之一",
+              coord: [121.614875, 25.239825]
+            }
+          ]
+        },
+        {
+          name: "王爺／千歲",
+          id: "wangye",
+          re: "千歲|王爺",
+          highlight: [
+            {
+              id: 1,
+              location: "台南北門",
+              temple: "南鯤鯓代天府",
+              description: "南瀛古三大代天府",
+              coord: [120.1430727, 23.2863908]
+            },
+            {
+              id: 2,
+              location: "澎湖",
+              temple: "澎湖縣",
+              description: "全澎王爺廟間數高居全澎廟宇之冠",
+              coord: [119.621856, 23.564766]
+            },
+            {
+              id: 3,
+              location: "金門",
+              temple: "金門縣",
+              description: "金門所供奉的王爺有四十多姓",
+              coord: [118.3799582, 24.447835]
+            },
+            {
+              id: 4,
+              location: "屏東東港",
+              temple: "東隆宮",
+              description: "以東港燒王船聞名",
+              coord: [120.4488088, 22.4630697]
+            }
+          ]
+        },
+        {
+          name: "玄天上帝",
+          id: "xuanwu",
+          re: "玄天|真武|北極大帝",
+          highlight: [
+            {
+              id: 1,
+              location: "南投名間",
+              temple: "松柏嶺受天宮",
+              description: "祀奉玄天上帝的總廟",
+              coord: [120.6302176, 23.8348126]
+            },
+            {
+              id: 2,
+              location: "雲林斗六",
+              temple: "斗六市",
+              description: "斗六玄天上帝廟宇間數全縣最多",
+              coord: [120.5409148, 23.7078134]
+            }
+          ]
+        }
       ],
       width: 300,
-      height: 400,
-      colorPalette: ["#eff3ff", "#bdd7e7", "#6baed6", "#3182bd", "#08519c"]
+      height: 410,
+      colorPalette: ["#eff3ff", "#bdd7e7", "#6baed6", "#2171b5"],
+      highlightLength: 40
     };
   },
   computed: {
@@ -79,6 +289,16 @@ export default {
         ])
       );
     },
+    polyline() {
+      return new Map(
+        this.godsList.map(d => {
+          const x = this.projection(d.highlight[0].coord)[0];
+          const y = this.projection(d.highlight[0].coord)[1];
+          const l = this.highlightLength;
+          return [d.id, `${x},${y} ${x - l},${y}`];
+        })
+      );
+    },
     background() {
       return topojson.feature(county, county.objects["tw-county"]);
     },
@@ -93,8 +313,6 @@ export default {
 <style scoped lang="scss">
 .mapWrapper {
   padding: 20px 0;
-  margin: auto;
-  max-width: 1000px;
   display: flex;
   justify-content: space-evenly;
   flex-wrap: wrap;
@@ -105,11 +323,14 @@ export default {
   padding-bottom: 50px;
 }
 
-.mapTitle {
+.mapText {
   position: absolute;
   left: 1rem;
   top: 1rem;
   color: black;
+  ol {
+    padding-inline-start: 1rem;
+  }
 }
 
 .background {
@@ -127,12 +348,38 @@ export default {
   stroke: none;
 }
 
+.highlight {
+  polyline {
+    stroke: black;
+    stroke-width: 0.75px;
+  }
+  text {
+    font-size: 0.75rem;
+    font-weight: 600;
+    text-anchor: middle;
+    dominant-baseline: middle;
+    fill: white;
+    paint-order: stroke;
+    stroke: black;
+    stroke-width: 0.75px;
+  }
+  circle {
+    fill: white;
+    stroke: black;
+    paint-order: stroke;
+    stroke-width: 0.75px;
+  }
+}
+
 @media (max-width: 480px) {
   .mapContainer {
     width: 90vw;
   }
-  .mapTitle {
+  .mapText {
     font-size: 0.8rem;
+    li {
+      font-size: 0.5rem;
+    }
   }
 }
 </style>
